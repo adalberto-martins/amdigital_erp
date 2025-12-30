d38960a8148fe04f435d2eee20b9bacddc350117
adalberto-martins <145614606+adalberto-martins@users.noreply.github.com>
2025-12-30 20:53:49 -0300

    Create home.php

---
+++ app/views/home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>AM Digital ERP</title>
+</head>
+<body>
+    <h1>ERP funcionando com MVC 🚀</h1>
+</body>
+</html>
