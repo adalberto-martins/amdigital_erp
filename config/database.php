85bb98e25f27cf1b693985232703489600e11b68
adalberto-martins <145614606+adalberto-martins@users.noreply.github.com>
2025-12-30 20:53:52 -0300

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php
+
+function conexao() {
+    $host = 'localhost';
+    $db   = 'amdigital';
+    $user = 'root';
+    $pass = '';
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+    try {
+        $pdo = new PDO($dsn, $user, $pass, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+        return $pdo;
+    } catch (PDOException $e) {
+        die('Erro de conexão: ' . $e->getMessage());
+    }
+}
