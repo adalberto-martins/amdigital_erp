6e75802d995a98d8c56ccbe9dfe29db00147b328
adalberto-martins <145614606+adalberto-martins@users.noreply.github.com>
2026-01-01 13:39:55 -0300

    Create script.js

---
+++ public/assets/js/script.js
@@ -0,0 +1,19 @@
+document.getElementById('cpf_cnpj').addEventListener('input', function (e) {
+  let value = e.target.value.replace(/\D/g, '');
+
+  if (value.length <= 11) {
+    // CPF
+    value = value.replace(/(\d{3})(\d)/, '$1.$2');
+    value = value.replace(/(\d{3})(\d)/, '$1.$2');
+    value = value.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+  } else {
+    // CNPJ
+    value = value.replace(/^(\d{2})(\d)/, '$1.$2');
+    value = value.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3');
+    value = value.replace(/\.(\d{3})(\d)/, '.$1/$2');
+    value = value.replace(/(\d{4})(\d)/, '$1-$2');
+  }
+
+  e.target.value = value;
+});
+
